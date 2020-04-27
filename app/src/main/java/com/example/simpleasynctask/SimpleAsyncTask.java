54d9aaeb3939d19ec99fb461d2c87a670e472e9e
Atthoriq Gerhana Firdaus <atthoriq.gerhana.tif18@polban.ac.id>
2020-04-27 12:15:34 +0700

    init commit

---
+++ app/src/main/java/com/example/simpleasynctask/SimpleAsyncTask.java
@@ -0,0 +1,44 @@
+package com.example.simpleasynctask;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
+
+public class SimpleAsyncTask extends AsyncTask <Void, Integer, String>{
+
+    private WeakReference<TextView> mTextView;
+    private WeakReference<TextView> mResultTextView;
+
+    SimpleAsyncTask(TextView tv, TextView result){
+        mTextView = new WeakReference<>(tv);
+        mResultTextView = new WeakReference<>(result);
+    }
+
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        Random r = new Random();
+        int n = r.nextInt(11);
+        int s = n * 200;
+        publishProgress(s);
+        try{
+            Thread.sleep(s);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        return "Awake at last after sleeping fo " + s + " milliseconds!";
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        mResultTextView.get().setText("Current sleep time: " + values[0] + " ms");
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+        mTextView.get().setText(result);
+    }
+}
